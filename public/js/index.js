28fe984c854c9ab6ba0c5dec3d9e2e98fefcb620
Olof Johansson <olof@liberdev.se>
2025-04-30 10:21:02 +0200

    Add loading animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,11 @@
 console.info("Hello from the console!");
+
+const spinner = document.querySelector(".spinner");
+
+const formTextField = document.querySelector("#question");
+
+document.getElementById("submitFormButton").addEventListener("click", () => {
+    if (formTextField.value) {
+        spinner.style.opacity = 1;
+    }
+});
--- src/templates/form.php
+++ src/templates/form.php
@@ -1,5 +1,6 @@
 <form action="/" method="GET">
     <label for="question">Say something to the LLM:</label><br>
     <textarea required id="question" name="question" rows="4" cols="50"><?= isset($question) ? htmlspecialchars($question) : null ?></textarea><br>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit" id="submitFormButton">
+    <div class="spinner"></div>
 </form>
