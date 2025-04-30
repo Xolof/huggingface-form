0cb685734f0199bffee9d02c5d741b7e64ff28b2
Olof Johansson <olof@liberdev.se>
2025-04-30 20:42:44 +0200

    Move some API related logic to the router
    We want to try and do the same for the other routes as the project progresses

--- src/app.php
+++ src/app.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . "/functions/functions.php";
+require __DIR__ . "/models/Api.php";
 
 $uri = $_SERVER['REQUEST_URI'];
 
@@ -11,17 +12,27 @@ if ($positionQuestionMark = strpos($uri, "?")) {
 
 switch ($uri) {
     case '/':
-        require __DIR__ . "/models/home.php";
+        $question = filter_input(INPUT_GET, 'question', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if (isset($question)) {
+            $api = new Api($question);
+            $res = $api->makeCurlRequest();
+            $markdown = makeMarkdown($res);
+        }
+        require __DIR__ . "/views/homeView.php";
         break;
+
     case '/blog':
-        require __DIR__ . "/models/blog.php";
+        require __DIR__ . "/views/blogView.php";
         break;
+
     case '/admin':
-        require __DIR__ . "/models/admin.php";
+        require __DIR__ . "/views/adminView.php";
         break;
+
     case '/login':
-        require __DIR__ . "/models/login.php";
+        require __DIR__ . "/views/loginView.php";
         break;
+
     default:
         http_response_code(404);
         echo '404 Not Found';
