7902ec097e13bb563c399db3ecde5ead803928f9
Olof Johansson <olof@liberdev.se>
2025-04-30 10:56:17 +0200

    Use strip tags instead of htmlspecialchars so that swedish characters can be output properly

--- src/templates/form.php
+++ src/templates/form.php
@@ -1,6 +1,6 @@
 <form action="/" method="GET">
     <label for="question">Say something to the LLM:</label><br>
-    <textarea required id="question" name="question" rows="4" cols="50"><?= isset($question) ? htmlspecialchars($question) : null ?></textarea><br>
+    <textarea required id="question" name="question" rows="4" cols="50"><?= isset($question) ? strip_tags($question) : null ?></textarea><br>
     <input type="submit" value="Submit" id="submitFormButton">
     <div class="spinner"></div>
 </form>
