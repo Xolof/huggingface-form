4755b872e391fffadc77427aff142fc59de73079
Olof Johansson <olof@liberdev.se>
2025-05-01 21:56:45 +0200

    echo null if there is no markdown

--- src/views/homeView.php
+++ src/views/homeView.php
@@ -4,6 +4,6 @@ require __DIR__ . "/../templates/header.php";
 
 require __DIR__ . "/../templates/apiForm.php";
 
-echo $markdown;
+echo $markdown ?? null;
 
 require __DIR__ . "/../templates/footer.html";
