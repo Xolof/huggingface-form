25a0869781f2ea812f8cbf67cc558a36ebcec3a9
Olof Johansson <olof@liberdev.se>
2025-05-01 20:07:06 +0200

    Add script for static analysis tools

---
+++ static_analysis_tools.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+pretty-php --space=4 --no-simplify-strings src 
+php-cs-fixer fix src
+phpcs src
+phpcbf src
+phpmd src text cleancode
+phpcpd src 
+phpstan analyse src --memory-limit="512M" --level=7
+
