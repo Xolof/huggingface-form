88171b8c483f50e814872079e96802523f09a558
Olof Johansson <olof@liberdev.se>
2025-05-01 22:21:23 +0200

    comment out phpcs and phpcbf from static analysis script for now

--- static_analysis_tools.sh
+++ static_analysis_tools.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 pretty-php --space=4 --no-simplify-strings src 
 php-cs-fixer fix src
-phpcs src
-phpcbf src
+#phpcs src
+#phpcbf src
 phpmd src text cleancode
 phpcpd src 
 phpstan analyse src --memory-limit="512M" --level=6
