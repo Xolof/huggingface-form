e26fde62dc56c41708f7b66f5e076e212b1498e4
Olof Johansson <olof@liberdev.se>
2025-05-02 12:45:44 +0200

    Install static analysis tools in the project as dev dependencies

--- static_analysis_tools.sh
+++ static_analysis_tools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-php-cs-fixer fix src
-phpcs src -s
-phpcbf src
-phpmd src text cleancode
-phpcpd src 
-phpstan analyse src --memory-limit="512M" --level=5
+vendor/bin/php-cs-fixer fix src
+vendor/bin/phpcs src -s
+vendor/bin/phpcbf src
+vendor/bin/phpmd src text cleancode
+vendor/bin/phpcpd src
+vendor/bin/phpstan analyse src --memory-limit="512M" --level=5
