cabe5bd9b8d821d73d777eb1271f9f4f6d986c13
Olof Johansson <olof@liberdev.se>
2025-05-02 13:21:46 +0200

    Install PhpUnit and remove phpcpd
    phpcpd is no longer maintained so it is best to remove it

--- static_analysis_tools.sh
+++ static_analysis_tools.sh
@@ -3,5 +3,4 @@ vendor/bin/php-cs-fixer fix src
 vendor/bin/phpcs src -s
 vendor/bin/phpcbf src
 vendor/bin/phpmd src text cleancode
-vendor/bin/phpcpd src
 vendor/bin/phpstan analyse src --memory-limit="512M" --level=5
