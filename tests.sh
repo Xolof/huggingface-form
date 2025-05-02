e4793c37df6d81081132af2e63caf7a0ed8944be
Olof Johansson <olof@liberdev.se>
2025-05-02 22:15:01 +0200

    Colorize phpunit output

--- tests.sh
+++ tests.sh
@@ -4,5 +4,4 @@ vendor/bin/phpcs src -s
 vendor/bin/phpcbf src
 vendor/bin/phpmd src text cleancode
 vendor/bin/phpstan analyse src --memory-limit="512M" --level=5
-vendor/bin/phpunit tests --coverage-html coverage/
-
+vendor/bin/phpunit tests --coverage-html coverage/ --colors
