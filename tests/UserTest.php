3f5436532832d7645bc0a8141140df6c1ac4414a
Olof Johansson <olof@liberdev.se>
2025-05-02 18:44:24 +0200

    Add some tests for the class User

---
+++ tests/UserTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use App\Models\Db;
+
+require_once __DIR__ . '/../config.php';
+
+final class UserTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $db = new Db();
+        $db->connect();
+        $db->runQuery("DELETE FROM users WHERE id > 1");
+    }
+
+    public function testCanCreate(): void
+    {
+        $user = new User();
+
+        $email = 'user@user.se';
+        $createResult = $user->create(
+            'user', $email, 0,
+            '$2y$12$Rqq/zGND.26gnXUF03a2DOPfSYk9/ueyHu1ObLM5LYDneVjML45ra'
+        );
+        var_dump($createResult);
+        $getResult = $user->getByEmail($email);
+        $this->assertSame("user", $getResult["name"]);
+    }
+
+    public function testCanNotCreateIfExists(): void
+    {
+        $this->expectException(\Exception::class);
+
+        $user = new User();
+        $email = 'user@user.se';
+        
+        $createResult = $user->create(
+            'user', $email, 0,
+            '$2y$12$Rqq/zGND.26gnXUF03a2DOPfSYk9/ueyHu1ObLM5LYDneVjML45ra'
+        );
+  
+        $createResult = $user->create(
+            'user', $email, 0,
+            '$2y$12$Rqq/zGND.26gnXUF03a2DOPfSYk9/ueyHu1ObLM5LYDneVjML45ra'
+        );
+    }
+
+    public function testCanGetByEmail(): void
+    {
+        $email = "oljo@protonmail.ch";
+
+        $user = new User();
+        $res = $user->getByEmail($email);
+
+        $this->assertSame("admin", $res["name"]);
+    }
+}
+
