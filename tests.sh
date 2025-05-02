99d55ef8077ca5653e458fc61d3a72bc05d897c8
Olof Johansson <olof@liberdev.se>
2025-05-02 21:56:37 +0200

    rename static_analysis_tools.sh -> tests.sh

--- tests.sh
+++ tests.sh
@@ -4,3 +4,5 @@ vendor/bin/phpcs src -s
 vendor/bin/phpcbf src
 vendor/bin/phpmd src text cleancode
 vendor/bin/phpstan analyse src --memory-limit="512M" --level=5
+vendor/bin/phpunit tests --coverage-html coverage/
+
