efff43286596129ae2a30e34083ed0f7e065ddd3
Olof Johansson <olof@liberdev.se>
2025-05-03 10:41:51 +0200

    Add test class for Post

---
+++ tests/PostTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Post;
+use App\Models\Db;
+
+#[CoversClass(Post::class)]
+final class PostTest extends TestCase
+{
+    protected function setUp(): void
+    {
+
+    }
+
+    protected function tearDown(): void
+    {
+        $db = new Db();
+        $db->connect();
+        $db->runQuery("DELETE FROM posts WHERE id > 2");
+    }
+
+    public function testCanAdd(): void
+    {
+        $post = new Post();
+        $text = "This is some text.";
+        $post->add(1, $text, 1736015394);
+        $allPosts = $post->getAll();
+
+        $this->assertSame($text, $allPosts[array_key_last($allPosts)]["post"]);
+    }
+}
