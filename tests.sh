9b8b9d9b5765ae2779dc7fcc2ff9502ecb02d14a
Olof Johansson <olof@liberdev.se>
2025-05-03 11:19:30 +0200

    Include tests directory in the static tests.

--- tests.sh
+++ tests.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
-vendor/bin/php-cs-fixer fix src
-vendor/bin/phpcs src -s
-vendor/bin/phpcbf src
+vendor/bin/php-cs-fixer fix src tests
+vendor/bin/phpcs src tests -s
+vendor/bin/phpcbf src tests
 vendor/bin/phpmd src text cleancode
-vendor/bin/phpstan analyse src --memory-limit="512M" --level=5
+vendor/bin/phpmd tests text cleancode
+vendor/bin/phpstan analyse src tests --memory-limit="512M" --level=5
 vendor/bin/phpunit tests --coverage-html coverage/ --colors
