84df6854a6b066bf0683640b83a8e6d178fa0435
Olof Johansson <olof@liberdev.se>
2025-05-04 17:41:55 +0200

    Get env variable with  instead of getenv() to make it work when executed through the CLI.

--- src/Models/Db.php
+++ src/Models/Db.php
@@ -16,7 +16,7 @@ class Db
 
     public function connect(): void
     {
-        $dbPath = getenv("SQLITE_DB_PATH");
+        $dbPath = $_ENV["SQLITE_DB_PATH"];
         $this->connection = new SQLite3($dbPath);
         $this->connection->enableExceptions(true);
     }
