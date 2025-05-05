3614ea13709a6baa1cc68c0097b739d2868a4f5b
Olof Johansson <olof@liberdev.se>
2025-05-05 17:17:07 +0200

    Make a table showing posts in the admin view

--- src/views/adminView.php
+++ src/views/adminView.php
@@ -16,4 +16,32 @@ require __DIR__ . "/../templates/header.php";
 
 require __DIR__ . "/../templates/schedulePostForm.php";
 
+$allPosts = $allPosts ?? null;
+?>
+
+<h3>Posts</h3>
+
+<table>
+<tr>
+    <th>Topic</th>
+    <th>Published</th>
+    <th>Time to publish</th>
+    <th></th>
+</tr>
+<?php foreach ($allPosts as $post) : ?>
+    <tr>
+        <td><?= $post["question"]; ?></td>
+        <td><?= $post["post"] === "" ? "No" : "Yes"; ?></td>
+        <td><?= date("Y-m-d H:i", $post["publish_unix_timestamp"]); ?></td>
+        <td><a href="/delete-post?id=<?= $post["post_id"]; ?>">Delete</a></td>
+    </tr>
+<?php endforeach; ?>
+</table>
+
+<?php if (count($allPosts) < 1) : ?>
+    <p>There are not yet any posts.</p>
+<?php endif; ?>
+
+<?php
+
 require __DIR__ . "/../templates/footer.html";
