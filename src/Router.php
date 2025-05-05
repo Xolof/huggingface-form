e037cfd5a5c5be15af6f5332c21a75025c54f2fa
Olof Johansson <olof@liberdev.se>
2025-05-05 10:19:31 +0200

    Add a simple Router class

---
+++ src/Router.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+class Router
+{
+    private $routes = [];
+
+    public function add($method, $path, $handler)
+    {
+        $this->routes[] = [
+            'method' => strtoupper($method),
+            'path' => $path,
+            'handler' => $handler
+        ];
+    }
+
+    public function get($path, $handler)
+    {
+        $this->add('GET', $path, $handler);
+    }
+
+    public function post($path, $handler)
+    {
+        $this->add('POST', $path, $handler);
+    }
+
+    public function dispatch($method, $uri)
+    {
+        $method = strtoupper($method);
+
+        foreach ($this->routes as $route) {
+            if ($route['method'] === $method && $route['path'] === $uri) {
+                return call_user_func($route['handler']);
+            }
+        }
+
+        http_response_code(404);
+        return '404 Not Found';
+    }
+}
