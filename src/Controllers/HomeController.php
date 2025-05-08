32435422cb8fe126d2cd8fa2e197dbde52166dfb
Olof Johansson <olof@liberdev.se>
2025-05-08 16:54:05 +0200

    Move logic under routes to controllers.

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Helpers\Logger;
+use App\Models\Api;
+use App\Clients\CurlHttpClient;
+use App\Helpers\Markdowner;
+
+class HomeController extends Controller
+{
+    public static function home(): void
+    {
+        $question = filter_input(INPUT_GET, 'question', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if (isset($question)) {
+
+            $token = $_ENV["HF_API_TOKEN"];
+            if (!$token) {
+                throw new \Exception("Could not get the API token.");
+            };
+
+            $logger = new Logger();
+            $curlHttpClient = new CurlHttpClient();
+
+            $api = new Api($question, $logger, $curlHttpClient, $token);
+            $res = $api->makeCurlRequest();
+            $markdowner = new Markdowner();
+            $markdown = $markdowner->print($res);
+        }
+        include __DIR__ . "/../views/homeView.php";
+    }
+}
