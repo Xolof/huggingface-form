1243cdbe012c7b58b447a0bd16a75fc862b39ef1
Olof Johansson <olof@liberdev.se>
2025-05-09 20:05:15 +0200

    Add custom 404 and 500 views

---
+++ src/views/404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+</head>
+<body>
+    <h1>404 Not Found</h1>
+    <p>Try going back to the <a href="/">start page</a>.</p>
+    <style>
+        body {
+            font-family: 'Courier New', Courier, monospace;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        h1 {
+            color: #202020;
+            border: 4px solid #4232d6;
+            border-radius: 8px;
+            padding: 1em;
+            text-align: center;
+        }
+    </style>
+</body>
+</html>
