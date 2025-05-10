1af31950493f2318215dd03eb4f73f99678a0108
Olof Johansson <olof@liberdev.se>
2025-05-10 15:08:18 +0200

    Add a phpmd config file and exclude the warning 'StaticAccess'

--- tests.sh
+++ tests.sh
@@ -2,8 +2,8 @@
 vendor/bin/php-cs-fixer fix src tests
 vendor/bin/phpcs src tests -s
 vendor/bin/phpcbf src tests
-vendor/bin/phpmd src text cleancode
-vendor/bin/phpmd tests text cleancode
+vendor/bin/phpmd src text phpmd.xml
+vendor/bin/phpmd tests text phpmd.xml
 vendor/bin/phpstan analyse src tests --memory-limit="512M" --level=5
 rm test.db
 sqlite3 test.db < setup.sql
